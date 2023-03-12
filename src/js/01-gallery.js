52ea7513a15aeaf028e8cd2495c5109dda1f319c
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-03-12 22:16:59 +0100

    zadania 1,3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" />
+      </a>
+    </div>
+  `;
+  })
+  .join('');
+
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('beforeend', gallery);
+
+galleryList.addEventListener('click', onGalleryClick);
+let activeIndex;
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  activeIndex = galleryItems.findIndex(
+    item => item.original === event.target.dataset.source
+  );
+
+  const instance = new SimpleLightbox(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.open();
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+const LOCALST_KEY = 'feedback-form-state';
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const state = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem(LOCALST_KEY, JSON.stringify(state));
+  }, 500)
+);
+
+const savedState = JSON.parse(localStorage.getItem(LOCALST_KEY));
+if (savedState) {
+  emailInput.value = savedState.email;
+  messageInput.value = savedState.message;
+}
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(state);
+  localStorage.removeItem(LOCALST_KEY);
+  emailInput.value = '';
+  messageInput.value = '';
+});
