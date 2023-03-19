16ae099f435b728ee94c90e7576a34b2e0c9428e
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-03-19 20:58:14 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,27 @@
-import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-const gallery = galleryItems
-  .map(({ preview, original, description }) => {
-    return `
-    <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" />
-      </a>
-    </div>
-  `;
-  })
-  .join('');
+const gallery = galleryItems.map(({ preview, original, description }) => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" />
+    </a>
+  </div>
+`).join('');
 
 const galleryList = document.querySelector('.gallery');
 galleryList.insertAdjacentHTML('beforeend', gallery);
 
-galleryList.addEventListener('click', onGalleryClick);
-let activeIndex;
-
-function onGalleryClick(event) {
+galleryList.addEventListener('click', event => {
   event.preventDefault();
   if (event.target.nodeName !== 'IMG') {
     return;
   }
-  activeIndex = galleryItems.findIndex(
-    item => item.original === event.target.dataset.source
-  );
-
+  
   const instance = new SimpleLightbox(`
     <img src="${event.target.dataset.source}" width="800" height="600">
   `);
 
   instance.open();
-}
+});
